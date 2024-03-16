7296b309eac1b1e140731d8a81c871fed3c381ee
guifriends <163153814+guifriends@users.noreply.github.com>
2024-03-16 17:37:32 +0000

    resolucao

--- exercicios/1-CRUD/4-consultar-dados.sql
+++ exercicios/1-CRUD/4-consultar-dados.sql
@@ -1,4 +1,23 @@
 -- Crie uma consulta que retorne apenas as colunas "Name", "Composer" e "AlbumId" da tabela "tracks"
-
+SELECT 
+name,
+composer,
+AlbumId
+FROM tracks;
 
 -- Repita a consulta anterior e dessa vez filtre os registros para que retornem apenas as tracks com o atributo "AlbumId" igual a 1
+SELECT 
+name,
+composer,
+albumId
+FROM tracks
+WHERE
+AlbumId=1;
+
+SELECT
+City,
+State
+FROM
+customers
+WHERE
+State='SP';
